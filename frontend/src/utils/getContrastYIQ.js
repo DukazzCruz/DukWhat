bb8d2a122a39bc7b54ad6e621bd7e5d4f85a8553
Luis Ducasse <ducassecruz@gmail.com>
2024-05-12 18:39:46 -0500

    Improve contrast logic

--- frontend/src/utils/getContrastYIQ.js
+++ frontend/src/utils/getContrastYIQ.js
@@ -1,8 +1,7 @@
+import chroma from 'chroma-js';
+
 export function getContrastYIQ(hexcolor){
-    hexcolor = hexcolor.replace("#", "");
-    const r = parseInt(hexcolor.substr(0,2),16);
-    const g = parseInt(hexcolor.substr(2,2),16);
-    const b = parseInt(hexcolor.substr(4,2),16);
-    const yiq = ((r*299)+(g*587)+(b*114))/1000;
-    return (yiq >= 128) ? 'black' : 'white';
+    const color = chroma(hexColor);
+    // Determina si el color es claro o oscuro
+    return color.luminance() > 0.5 ? 'black' : 'white';
 }
