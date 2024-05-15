b4bf69bb5c65ca476a23b39bb57cbb56bf60af85
Luis Ducasse <ducassecruz@gmail.com>
2024-05-15 00:46:32 +0000

    "Update Mustache helper and WbotService to include contact in edited message"
    This diff includes changes to the Mustache helper function to accept a Contact object as a parameter and to the EditWhatsAppMessage function in WbotServices to

--- backend/src/helpers/Mustache.ts
+++ backend/src/helpers/Mustache.ts
@@ -1,33 +1,31 @@
 import Mustache from "mustache";
 import Contact from "../models/Contact";
 
-
 // Función para determinar el saludo según la hora del día
 function getGreeting(): string {
-  const timeZone = process.env.TZ || 'America/Fortaleza';
-  const hour:number =  parseInt(
-    new Intl.DateTimeFormat('en-US', {
-      hour: 'numeric',
+  const timeZone = process.env.TZ || "America/Fortaleza";
+  const hour: number = parseInt(
+    new Intl.DateTimeFormat("en-US", {
+      hour: "numeric",
       hour12: false,
-      timeZone: timeZone
+      timeZone
     }).format(new Date()),
     10
   );
 
   if (hour < 12) {
     return "Buenos días";
-  } else if (hour < 18) {
-    return "Buenas tardes";
-  } else {
-    return "Buenas noches";
   }
+  if (hour < 18) {
+    return "Buenas tardes";
+  }
+  return "Buenas noches";
 }
 
 export default (body: string, contact: Contact): string => {
   const view = {
     name: contact ? contact.name || contact.number : "",
-    greeting: getGreeting(), // Incluye el saludo dinámico
-
+    greeting: getGreeting() // Incluye el saludo dinámico
   };
   return Mustache.render(body, view);
 };
--- backend/src/services/WbotServices/EditWhatsAppMessage.ts
+++ backend/src/services/WbotServices/EditWhatsAppMessage.ts
@@ -1,15 +1,24 @@
 import AppError from "../../errors/AppError";
 import GetWbotMessage from "../../helpers/GetWbotMessage";
+import Contact from "../../models/Contact";
 import Message from "../../models/Message";
 import Ticket from "../../models/Ticket";
+import formatBody from "../../helpers/Mustache";
 
-const EditWhatsAppMessage = async (messageId: string, newBody: string): Promise<Message> => {
+const EditWhatsAppMessage = async (
+  messageId: string,
+  newBody: string
+): Promise<Message> => {
   const message = await Message.findByPk(messageId, {
     include: [
       {
         model: Ticket,
         as: "ticket",
         include: ["contact"]
+      },
+      {
+        model: Contact,
+        as: "contact"
       }
     ]
   });
@@ -17,27 +26,32 @@ const EditWhatsAppMessage = async (messageId: string, newBody: string): Promise<
   if (!message) {
     throw new AppError("No message found with this ID.");
   }
+  const hasBody = newBody
+    ? formatBody(newBody as string, message.contact)
+    : undefined;
 
+  if (!hasBody) {
+    throw new AppError("No message found with this newBody.");
+  }
   const { ticket } = message;
 
   const messageToEdit = await GetWbotMessage(ticket, messageId);
 
   try {
-    const res = await messageToEdit.edit(newBody);
-    if (res === null)
-      throw new Error("Can't edit");
+    const res = await messageToEdit.edit(hasBody);
+    if (res === null) throw new Error("Can't edit");
   } catch (err) {
     throw new AppError("ERR_EDITING_WAPP_MSG");
   }
-  await message.update({ body: newBody });
+  await message.update({ body: hasBody });
   if (ticket.lastMessage === message.body) {
-    await ticket.update({ lastMessage: newBody });
+    await ticket.update({ lastMessage: hasBody });
   }
 
   // Buscar el mensaje más reciente en el ticket
   const mostRecentMessage = await Message.findOne({
     where: { ticketId: ticket.id },
-    order: [['updatedAt', 'DESC']]
+    order: [["updatedAt", "DESC"]]
   });
 
   // Si el mensaje más reciente es el que estamos editando
