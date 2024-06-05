3a5ea0b2164dd858d2f5cc2e3c18c2bd25d539c9
Luis Ducasse <luisducasse@larasofts.com>
2024-06-05 15:21:42 +0000

    Revert ""Added async-mutex dependency and implemented mutexes in FindOrCreateTicketService; updated MessagesList and TicketListItem components; added translations for message types""
    This reverts commit e438653b00234ee7822b9d676b6dfd6aa1c12e7b.

--- backend/src/services/TicketServices/FindOrCreateTicketService.ts
+++ backend/src/services/TicketServices/FindOrCreateTicketService.ts
@@ -1,21 +1,9 @@
 import { subHours } from "date-fns";
 import { Op } from "sequelize";
-import { Mutex } from "async-mutex";
 import Contact from "../../models/Contact";
 import Ticket from "../../models/Ticket";
 import ShowTicketService from "./ShowTicketService";
 import Whatsapp from "../../models/Whatsapp";
-
-// Mapa para almacenar mutexes dinámicamente
-const mutexes = new Map();
-
-const getMutex = (whatsappId: number, contactId: number) => {
-  const key = `${whatsappId}-${contactId}`;
-  if (!mutexes.has(key)) {
-    mutexes.set(key, new Mutex());
-  }
-  return mutexes.get(key);
-};
 
 const FindOrCreateTicketService = async (
   contact: Contact,
@@ -23,19 +11,30 @@ const FindOrCreateTicketService = async (
   unreadMessages: number,
   groupContact?: Contact
 ): Promise<Ticket> => {
-  const contactId = groupContact ? groupContact.id : contact.id;
-  const mutex = getMutex(whatsappId, contactId);
+  let ticket = await Ticket.findOne({
+    where: {
+      status: {
+        [Op.or]: ["open", "pending"]
+      },
+      contactId: groupContact ? groupContact.id : contact.id,
+      whatsappId
+    },
+    include: [
+      {
+        model: Whatsapp,
+        attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
+      }
+    ]
+  });
 
-  let ticket;
+  if (ticket) {
+    await ticket.update({ unreadMessages });
+  }
 
-  await mutex.runExclusive(async () => {
-    // Verifica si ya existe un ticket abierto para este contacto y conexión
+  if (!ticket && groupContact) {
     ticket = await Ticket.findOne({
       where: {
-        status: {
-          [Op.or]: ["open", "pending"]
-        },
-        contactId,
+        contactId: groupContact.id,
         whatsappId
       },
       include: [
@@ -43,90 +42,66 @@ const FindOrCreateTicketService = async (
           model: Whatsapp,
           attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
         }
-      ]
+      ],
+      order: [["updatedAt", "DESC"]]
     });
 
     if (ticket) {
-      //@ts-ignore
-      await ticket.update({ unreadMessages });
-      return;
-    }
-
-    if (!ticket && groupContact) {
-      ticket = await Ticket.findOne({
-        where: {
-          contactId: groupContact.id,
-          whatsappId
-        },
-        include: [
-          {
-            model: Whatsapp,
-            attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
-          }
-        ],
-        order: [["updatedAt", "DESC"]]
+      await ticket.update({
+        status: "pending",
+        userId: null,
+        unreadMessages
       });
-
-      if (ticket) {
-        await ticket.update({
-          status: "pending",
-          userId: null,
-          unreadMessages
-        });
-        return;
-      }
     }
+  }
 
-    if (!ticket && !groupContact) {
-      ticket = await Ticket.findOne({
-        where: {
-          updatedAt: {
-            [Op.between]: [+subHours(new Date(), 2), +new Date()]
-          },
-          contactId: contact.id,
-          whatsappId
+  if (!ticket && !groupContact) {
+    ticket = await Ticket.findOne({
+      where: {
+        updatedAt: {
+          [Op.between]: [+subHours(new Date(), 2), +new Date()]
         },
-        include: [
-          {
-            model: Whatsapp,
-            attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
-          }
-        ],
-        order: [["updatedAt", "DESC"]]
-      });
-
-      if (ticket) {
-        await ticket.update({
-          status: "pending",
-          userId: null,
-          unreadMessages
-        });
-        return;
-      }
-    }
-
-    if (!ticket) {
-      ticket = await Ticket.create(
+        contactId: contact.id,
+        whatsappId
+      },
+      include: [
         {
-          contactId: groupContact ? groupContact.id : contact.id,
-          status: "pending",
-          isGroup: !!groupContact,
-          unreadMessages,
-          whatsappId
-        },
-        {
-          include: [
-            {
-              model: Whatsapp,
-              attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
-            }
-          ]
+          model: Whatsapp,
+          attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
         }
-      );
-    }
-  });
+      ],
+      order: [["updatedAt", "DESC"]]
+    });
 
-  //@ts-ignore
+    if (ticket) {
+      await ticket.update({
+        status: "pending",
+        userId: null,
+        unreadMessages
+      });
+    }
+  }
+
+  if (!ticket) {
+    ticket = await Ticket.create(
+      {
+        contactId: groupContact ? groupContact.id : contact.id,
+        status: "pending",
+        isGroup: !!groupContact,
+        unreadMessages,
+        whatsappId
+      },
+      {
+        include: [
+          {
+            model: Whatsapp,
+            attributes: ["color"] // Only fetch the color attribute from the Whatsapp model
+          }
+        ]
+      }
+    );
+  }
+
   ticket = await ShowTicketService(ticket.id);
 
   return ticket;
